28667729601132f58738ff2029ebe5c7651be0bd
lbh0830 <qwqsqx30@gmail.com>
2018-06-26 15:22:23 +0800

    update sql

--- shop.sql
+++ shop.sql
@@ -1,8 +1,10 @@
--- MySQL dump 10.13  Distrib 5.7.22, for Win64 (x86_64)
+CREATE DATABASE  IF NOT EXISTS `shop` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `shop`;
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
 -- Host: localhost    Database: shop
 -- ------------------------------------------------------
--- Server version	5.7.22-log
+-- Server version	5.7.21-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -23,13 +25,15 @@ DROP TABLE IF EXISTS `commodity`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `commodity` (
+  `id` varchar(45) COLLATE utf8mb4_bin NOT NULL,
   `name` varchar(80) COLLATE utf8mb4_bin NOT NULL,
   `category` varchar(40) COLLATE utf8mb4_bin NOT NULL,
   `price` int(11) NOT NULL,
   `quantity` int(11) NOT NULL,
   `detail` varchar(200) COLLATE utf8mb4_bin DEFAULT NULL,
   `spec` varchar(40) COLLATE utf8mb4_bin NOT NULL,
-  `image` varchar(40) COLLATE utf8mb4_bin NOT NULL
+  `image` varchar(40) COLLATE utf8mb4_bin NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -39,6 +43,7 @@ CREATE TABLE `commodity` (
 
 LOCK TABLES `commodity` WRITE;
 /*!40000 ALTER TABLE `commodity` DISABLE KEYS */;
+INSERT INTO `commodity` VALUES ('20180625001','鉛筆盒','配件',70,10,'精美的鉛筆盒，買到賺到哦!!!','黑','1529993011308.jpg'),('20180626001','禹丞的雙下巴','配件',50,1,'全球限量，僅此一件。偽裝肥宅必備武器，錯過了就買不到囉!! 快叫把拔買給你','一組兩件','1529993983176.jpg'),('20180626002','天線寶寶','配件',40,4,'一隻40，整組帶走算你200','紅綠黃紫','1529994751564.jpg');
 /*!40000 ALTER TABLE `commodity` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -60,7 +65,7 @@ CREATE TABLE `member` (
   `privilege` int(11) NOT NULL,
   PRIMARY KEY (`uid`),
   UNIQUE KEY `account` (`account`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -69,7 +74,7 @@ CREATE TABLE `member` (
 
 LOCK TABLES `member` WRITE;
 /*!40000 ALTER TABLE `member` DISABLE KEYS */;
-INSERT INTO `member` VALUES (1,'root','123456','12007','internet','12007','root@12007',31),(2,'test','123456','Henry','台北101101樓','0911333999','rrr@yahoo.mail.com',0),(3,'memberadmin','123456','administrator','internet','12007','root@12007',14);
+INSERT INTO `member` VALUES (1,'root','123456','12007','internet','12007','rootroot@12007',31),(2,'test','654321','Henry','台北101101樓','0911333999','rrr@yahoo.mail.com',0);
 /*!40000 ALTER TABLE `member` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -82,4 +87,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-06-25  0:29:48
+-- Dump completed on 2018-06-26 15:20:18
